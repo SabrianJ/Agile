49c1917fac9de220fa08bf40e9c1414abf9868de
SabrianJ <81123591+SabrianJ@users.noreply.github.com>
2021-10-20 12:51:20 +1300

    Invitation bug fixed

--- WoofApplication/public/global.js
+++ WoofApplication/public/global.js
@@ -79,6 +79,33 @@ function clearOverlays() {
   markersArray.length = 0;
 }
 
+function loadPicture(event){
+  var profileImage = document.getElementById("profileImage");
+  profileImage.src = URL.createObjectURL(event.target.files[0]);
+
+  getDataUri(URL.createObjectURL(event.target.files[0]), function(dataUri) {
+    var dataURIHolder = document.getElementById("imageURI");
+    dataURIHolder.value = dataUri;
+  });
+
+  profileImage.onload = function(){
+    URL.revokeObjectURL(profileImage.src)
+  }
+}
+
+function alternateImage(event){
+  const target = event.target;
+  target.src = "/img/undraw_profile.svg";
+}
+
+function joinInvitation(event, invitationID,type){
+  window.open("/join/" + type + "/" + invitationID,"_self");
+}
+
+function deleteInvitation(event, invitationID){
+  window.open("/delete/invitation/" + invitationID,"_self");
+}
+
 function isIconMouseEvent(e) {
   return "placeId" in e;
 }
